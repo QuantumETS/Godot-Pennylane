2036b20e6c62f837d2f4792c22e3f874794a6f50
Kuift Kotar <kuiftk@gmail.com>
2025-01-25 05:32:46 -0500

    cleanup gdextension template code

--- godot-quantum-rust/src/lib.rs
+++ godot-quantum-rust/src/lib.rs
@@ -1,73 +1,7 @@
 use godot::prelude::*;
-use godot::engine::Sprite2D;
-use godot::engine::ISprite2D;
 
-mod QuantumCircuit;
+mod QuantumCircuit; // libs.rs is godot's entry point, mod QuantumCircuit make our QuantumCircuit visible to godot
 struct PennylaneGodotRust;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for PennylaneGodotRust {}
-
-
-//Everything after this comment is not used, it's an example of how to interface within godot
-//in this case
-
-#[derive(GodotClass)]
-#[class(base=Sprite2D)]
-struct Player {
-    speed: f64,
-    angular_speed: f64,
-
-    base: Base<Sprite2D>
-}
-
-#[godot_api]
-impl ISprite2D for Player {
-    fn init(base: Base<Sprite2D>) -> Self {
-        godot_print!("Hello, world!"); // Prints to the Godot console
-        
-        Self {
-            speed: 1.0,
-            angular_speed: std::f64::consts::PI,
-            base,
-        }
-    }
-    fn physics_process(&mut self, delta: f64) {
-        // In GDScript, this would be: 
-        // rotation += angular_speed * delta
-        
-        let radians = (self.angular_speed * delta * self.speed) as f32;
-        self.base_mut().rotate(radians);
-        // The 'rotate' method requires a f32, 
-        // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
-        let rotation = self.base().get_rotation();
-        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
-        //self.base_mut().translate(velocity * delta as f32);
-        
-        // or verbose: 
-        // let this = self.base_mut();
-        // this.set_position(
-        //     this.position() + velocity * delta as f32
-        // );
-    }
-}
-
-#[godot_api]
-impl Player {
-    #[func]
-    fn increase_speed(&mut self, amount: f64) {
-        self.speed += amount;
-        self.base_mut().emit_signal("speed_increased".into(), &[]);
-    }
-    #[func]
-    fn get_speed(&mut self) -> f64 {
-        self.speed
-    }
-    #[func]
-    fn get_test_string(&mut self) -> GString{
-        "potatoes2".to_string().into()
-    }
-
-    #[signal]
-    fn speed_increased();
-}
