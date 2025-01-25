e432afee11ad475152992cc60ff722428351c5cc
Kuift Kotar <kuiftk@gmail.com>
2025-01-25 03:52:02 -0500

    update godot scene

--- godot/scenes/QuantumCircuit.gd
+++ godot/scenes/QuantumCircuit.gd
@@ -4,11 +4,14 @@ extends QuantumCircuit
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	init_circuit(5,1)
-	x(1)
+	x(0)
 	x(2)
+	h(1)
+	h(3)
 	var muh_measure = measure_all()
 	print("here are the results : ")
 	print(muh_measure)
+	get_expectation_value('z')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
