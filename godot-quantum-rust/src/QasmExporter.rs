b811a045f697f3c09a70529660e29c2563843f03
Kuift Kotar <kuiftk@gmail.com>
2025-02-01 19:11:58 -0500

    added working implementation of add_single_qubit_gate, add_controlled_qubit_gate and export_qasm

--- godot-quantum-rust/src/QasmExporter.rs
+++ godot-quantum-rust/src/QasmExporter.rs
@@ -1,3 +1,8 @@
+use std::path::Path;
+
+use godot::log::godot_print;
+use openqasm::{GenericError, Parser, SourceCache};
+
 #[derive(Default)]
 pub struct QasmExporterStruct {
     qasm_code: Vec<String>,
@@ -11,19 +16,39 @@ impl QasmExporterStruct {
         self.qasm_code.push(format!("qreg q[{}];", nb_qubits));
         self.qasm_code.push(format!("creg c[{}];", nb_bits));
     }
-    pub fn add_gate(&mut self, gate: &str, qubits: &[i64], value: Option<f64>) {
+    pub fn add_single_qubit_gate(&mut self, gate: &str, qubits_nb: &i64, value: Option<f64>) {
         let qasm_instruction = match value {
-            Some(v) => format!("{} {}({})", gate, qubits.iter().map(|q| q.to_string()).collect::<Vec<_>>().join(","), v),
-            None => format!("{} {}", gate, qubits.iter().map(|q| q.to_string()).collect::<Vec<_>>().join(",")),
+            Some(v) => format!("{}({}) q{};", gate, v, qubits_nb.to_string()),
+            None => format!("{} q{};", gate, qubits_nb.to_string()),
         };
         self.qasm_code.push(qasm_instruction);
     }
-
+    pub fn add_controlled_qubit_gate(&mut self, gate: &str, control: &i64, target: &i64, value: Option<f64>)
+    {
+        let qasm_instruction = match value {
+            Some(v) => format!("{}({}) q{}, q{};", gate, v, control.to_string(), target.to_string()),
+            None => format!("{} q{}, q{};", gate, control.to_string(), target.to_string()),
+        };
+        self.qasm_code.push(qasm_instruction);
+    }
     pub fn add_measurement(&mut self, qubit: i64) {
         self.qasm_code.push(format!("measure {}", qubit));
     }
 
     pub fn export_qasm(&self) -> String {
-        self.qasm_code.join("\n")
+        let code = self.qasm_code.join("\n");
+        // following might be better placed in a integration test : this test whether or not the generated qasm code is good
+        let mut cache = SourceCache::new();
+        let mut parser = Parser::new(&mut cache);
+        parser.parse_source(code.clone(), None::<&Path>);
+        match parser.done().to_errors() {
+            Ok(program) => {
+                if let Err(errors) = program.type_check().to_errors() {
+                    godot_print!("qasm code generation error, invalid type : {:?}", errors)
+                }
+            },
+            Err(errors) => godot_print!("qasm code generation error, invalid source : {:?}", errors),
+        }
+        code
     }
 }
