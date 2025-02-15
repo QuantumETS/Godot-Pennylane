5f0497edc932a32a493edf47c8f37a4114e4c115
Kuift Kotar <kuiftk@gmail.com>
2025-02-14 22:36:31 -0500

    renamed function w better name

--- godot/quantum_objects/bloch_sphere.gd
+++ godot/quantum_objects/bloch_sphere.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-func apply_theta_phi(real_1,im_1,real_2,im_2):
+func set_bloch_sphere_to_statevector(real_1,im_1,real_2,im_2):
 	var theta = 2 * acos(sqrt(real_1 ** 2 + im_1 ** 2)) 
 	var phi = atan2(im_2, real_2) - atan2(im_1, real_1)  
 	$MeshInstance3D/Anchor_node.rotation = Vector3(theta,phi+PI/2,0.0)
--- godot/scenes/QuantumCircuit.gd
+++ godot/scenes/QuantumCircuit.gd
@@ -20,6 +20,6 @@ func update_1_qubit_rxrz_circuit(rx,rz):
 	var real_2 = st["bases"][1]["re"]
 	var im_2 = st["bases"][1]["im"]
 	 
-	$bloch_sphere.apply_theta_phi(real_1,im_1,real_2,im_2)
+	$bloch_sphere.set_bloch_sphere_to_statevector(real_1,im_1,real_2,im_2)
 	# todo : vérifier équivalence rx(pi) et paulix, plus similaire à un ry
 	# représenter la phase avec les spinors 
