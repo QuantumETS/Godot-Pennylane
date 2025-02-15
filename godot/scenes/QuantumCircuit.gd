003f336ff155eafab53bcda6b0d18d74e295f92b
Kuift Kotar <kuiftk@gmail.com>
2025-02-14 23:49:34 -0500

    added display of the current statevector

--- godot/scenes/QuantumCircuit.gd
+++ godot/scenes/QuantumCircuit.gd
@@ -1,12 +1,16 @@
 extends QuantumCircuit
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func update_statevector_text(real1,im1,real2,im2):
+	# im1 will always be 0
+	var plus1 = "+" # stuff to make string formatting prettier
+	var plus2 = "+"
+	if real2 < 0: 
+		plus1 = "-"
+		real2 = abs(real2)
+	if im2 < 0: 
+		plus2 = "-"
+		im2 = abs(im2)
+	$"../Menu/statevector_txt".text = "Statevector : %.2f ∣0⟩ %s %.2f %s %.2fi ∣1⟩" % [real1,plus1,real2,plus2,im2]
 
 func update_1_qubit_rxrz_circuit(rx,rz):
 	init_circuit(1,1)
@@ -19,7 +23,17 @@ func update_1_qubit_rxrz_circuit(rx,rz):
 	var im_1 = st["bases"][0]["im"]
 	var real_2 = st["bases"][1]["re"]
 	var im_2 = st["bases"][1]["im"]
-	 
+	update_statevector_text(real_1,im_1,real_2,im_2)
+	print("im1:")
+	print(im_1)
+	print("im2:")
+	print(im_2)
 	$bloch_sphere.set_bloch_sphere_to_statevector(real_1,im_1,real_2,im_2)
 	# todo : vérifier équivalence rx(pi) et paulix, plus similaire à un ry
 	# représenter la phase avec les spinors 
+	# afficher le statevector sur l'écran
+	# afficher theta et phi sur le bloch sphere
+	# afficher l'ordre du circuit simuler et donner la possibilité de changer l'ordre
+	# ajouter ry
+	# afficher |+> |-> sur l'axe des x , |i> |-i> sur l'axe des y au bout du vecteur
+	
