14c2d3f4115bfc535c5c3ca9f88cf8e80cb69f43
Kuift Kotar <kuiftk@gmail.com>
2025-02-15 23:59:01 -0500

    added ability to reorder rx ry rz circuit

--- godot/scenes/UI/draggable_container.gd
+++ godot/scenes/UI/draggable_container.gd
@@ -1,10 +1,7 @@
 extends HBoxContainer
-
-func _ready():
-	$up.pressed.connect(_move_up)
-
 func _move_up():
 	var parent = get_parent()
 	var index = parent.get_children().find(self)
 	if index > 0:  # Prevent moving above the first element
 		parent.move_child(self, index - 1)
+		$"../..".update_the_bloch()
--- godot/scenes/UI/orderable_sliders.gd
+++ godot/scenes/UI/orderable_sliders.gd
@@ -5,16 +5,31 @@ var rz = 0.0
 var gate_order = ["rx", "ry", "rz"]
 func _on_rx_slider_value_changed(value: float) -> void:
 	rx = value*2*PI/360
+	gate_order = get_gate_order()
 	$"../../QuantumCircuit".update_1_qubit_circuit(rx,ry,rz,gate_order)
 	$VBoxContainer/rx_box/rx_text.text = "RX(%.6f)" % (rx)
 
-
 func _on_ry_slider_value_changed(value: float) -> void:
 	ry = value*2*PI/360
+	gate_order = get_gate_order()
 	$"../../QuantumCircuit".update_1_qubit_circuit(rx,ry,rz,gate_order)
 	$VBoxContainer/ry_box/ry_text.text = "RY(%.6f)" % (ry)
 
 func _on_rz_slider_value_changed(value: float) -> void:
 	rz = value*2*PI/360
+	gate_order = get_gate_order()
 	$"../../QuantumCircuit".update_1_qubit_circuit(rx,ry,rz,gate_order)
 	$VBoxContainer/rz_box/rz_text.text = "RZ(%.6f)" % (rz)
+
+func update_the_bloch() -> void:
+	gate_order = get_gate_order()
+	$"../../QuantumCircuit".update_1_qubit_circuit(rx,ry,rz,gate_order)
+
+func get_gate_order() -> Array:
+	var suffix = "_box"
+	var matching_children = []
+	for child in $VBoxContainer.get_children():
+		if child.name.ends_with(suffix):
+			matching_children.append(child.name.substr(0, 2))
+
+	return matching_children
