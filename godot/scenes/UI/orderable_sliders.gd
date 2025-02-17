56e9588b15e530ca4355775b0f18aa9f4595a5c1
Kuift Kotar <kuiftk@gmail.com>
2025-02-17 03:28:32 -0500

    added edit field for exact rotation value

--- godot/scenes/UI/orderable_sliders.gd
+++ godot/scenes/UI/orderable_sliders.gd
@@ -5,25 +5,23 @@ var rz = 0.0
 var gate_order = ["rx", "ry", "rz"]
 func _on_rx_slider_value_changed(value: float) -> void:
 	rx = value*2*PI/360
-	gate_order = get_gate_order()
-	$"../../QuantumCircuit".update_1_qubit_circuit(rx,ry,rz,gate_order)
-	$VBoxContainer/rx_box/rx_text.text = "RX(%.6f)" % (rx)
+	update_the_bloch()
 
 func _on_ry_slider_value_changed(value: float) -> void:
 	ry = value*2*PI/360
-	gate_order = get_gate_order()
-	$"../../QuantumCircuit".update_1_qubit_circuit(rx,ry,rz,gate_order)
-	$VBoxContainer/ry_box/ry_text.text = "RY(%.6f)" % (ry)
+	update_the_bloch()
+
 
 func _on_rz_slider_value_changed(value: float) -> void:
 	rz = value*2*PI/360
-	gate_order = get_gate_order()
-	$"../../QuantumCircuit".update_1_qubit_circuit(rx,ry,rz,gate_order)
-	$VBoxContainer/rz_box/rz_text.text = "RZ(%.6f)" % (rz)
+	update_the_bloch()
 
 func update_the_bloch() -> void:
 	gate_order = get_gate_order()
 	$"../../QuantumCircuit".update_1_qubit_circuit(rx,ry,rz,gate_order)
+	$VBoxContainer/rx_box/rx_text.text = "RX(%.6f)" % (rx)
+	$VBoxContainer/ry_box/ry_text.text = "RY(%.6f)" % (ry)
+	$VBoxContainer/rz_box/rz_text.text = "RZ(%.6f)" % (rz)
 
 func get_gate_order() -> Array:
 	var suffix = "_box"
@@ -33,3 +31,18 @@ func get_gate_order() -> Array:
 			matching_children.append(child.name.substr(0, 2))
 
 	return matching_children
+
+
+func _on_setrx_text_changed(value: String) -> void:
+	rx = value.to_float()
+	update_the_bloch()
+
+
+func _on_setry_text_changed(value: String) -> void:
+	ry = value.to_float()
+	update_the_bloch()
+
+
+func _on_setrz_text_changed(value: String) -> void:
+	rz = value.to_float()
+	update_the_bloch()
